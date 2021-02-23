3dba02e7bd3323fa560a821ff61258e6b49fbdd7
pavan koka <pavan.koka@toppr.com>
2021-02-24 00:00:48 +0530

    animations added

--- src/modules/home/components/home.js
+++ src/modules/home/components/home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import styles from './home.module.scss';
 
 function Home() {
     return (
         <div className={styles.wrapper}>
+            <div className={styles.particles}><Particles /></div>
             <img className={styles.image} src={require('assets/images/3d-image.png')} alt='3d' />
             <p className={styles.header}>useful technologies</p>
         </div>
