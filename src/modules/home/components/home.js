0ca164df4d1b8ddaa0fce6401862fe4ba0fe03e3
pavan koka <pavan.koka@toppr.com>
2021-02-24 18:22:46 +0530

    added animations

--- src/modules/home/components/home.js
+++ src/modules/home/components/home.js
@@ -5,9 +5,15 @@ import styles from './home.module.scss';
 function Home() {
     return (
         <div className={styles.wrapper}>
-            <div className={styles.particles}><Particles /></div>
+            <div className={styles.particles}>
+                <Particles
+                    width={1200}
+                    height={600}
+                />
+            </div>
             <img className={styles.image} src={require('assets/images/3d-image.png')} alt='3d' />
             <p className={styles.header}>useful technologies</p>
+            <img className={styles.scroll} src={require('assets/images/scroll.gif')} alt='3d' />
         </div>
     )
 }
